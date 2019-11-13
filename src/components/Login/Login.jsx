ecff3331fa299da362fc5a18d84f7b64dc619d83
Elijah McKay <elijahmckay10@gmail.com>
2019-11-13 08:06:25 -0600

    signup and login components created on styled

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
+// style sheet import
+import './Login.scss'; 
+
 const Login = () => {
     return ( 
         <div className="login-container">
             <h1 className="login-heading">Login</h1>
             <form className="form-container">
-                <label className="form-label">Email
+                <label className="form-label">Email<br /> 
                     <input type="text" className="form-input"/>
                 </label>
-                <label className="form-label">Password
+                <div className="divider"></div>
+                <label className="form-label">Password<br />
                     <input type="password" className="form-input"/>
                 </label>
                 <div className="under-input">
                     <div className="remember-me-container">
-                        <label className="remember-me-label">Remember Me
+                        <label className="remember-me-label">Remember Me<br />
                             <input type="checkbox" className="checkbox"/>
                         </label>
                     </div>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
+// style sheet import
+import './SignUp.scss'; 
+
 const SignUp = () => {
     return ( 
-        <>
-            <p>Sign Up</p>
-        </>
+        <div className="signup-container">
+            <h1 className="signup-heading">Sign Up</h1>
+            <form className="form-container">
+                <label className="form-label">Email<br /> 
+                    <input type="text" className="form-input"/>
+                </label>
+                <div className="divider"></div>
+                <label className="form-label">Password<br />
+                    <input type="password" className="form-input"/>
+                </label>
+                {/* <div className="under-input">
+                    <div className="remember-me-container">
+                        <label className="remember-me-label">Remember Me<br />
+                            <input type="checkbox" className="checkbox"/>
+                        </label>
+                    </div>
+                    <p className="forgot-password"><u>Forgot Password?</u></p>
+                </div> */}
+                <button className="signup-btn">Register</button>
+                <p className="create-an-account"><u>Sign In</u></p>
+            </form>
+        </div>
      );
 }
  
