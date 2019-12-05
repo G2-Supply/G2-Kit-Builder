a90eb2c7f99ea8029b02e36b02f0b3e33dd95cd0
Elijah McKay <elijahmckay10@gmail.com>
2019-12-05 07:52:25 -0600

    reformatted jsx element attributes to be more readable

--- src/components/DesignYourPallet/DesignYourPallet.jsx
+++ src/components/DesignYourPallet/DesignYourPallet.jsx
@@ -21,6 +21,7 @@ const DesignYourPallet = () => {
         styleOfTopBoards: '', 
         qtyOfTopBoards: '',
         lengthOfDeckBoards: '',
+        styleOfBottomBoards: '',
     })
 
     const changeHandler = (e) => {
@@ -36,32 +37,52 @@ const DesignYourPallet = () => {
             <h1 className="step-1-heading">Step 1 - Build Your Pallet</h1>
             <div className="input-wrapper">
                 <label htmlFor="minimizer" className="form-label">Skip this step</label>
-                <input type="checkbox" className="label-input" name="minimizer" id="minimizer" onChange={changeHandler} />
+                <input type="checkbox" 
+                    className="label-input" 
+                    name="minimizer" 
+                    id="minimizer" 
+                    onChange={changeHandler} />
             </div>
             <h2 className="runner-specifications">Runner Specifications</h2>
             <div className="line-1">
                 <div className="style-of-runner-container line-1-input">
                     <label htmlFor="style-of-runner" className="form-label">Style of Runner<br /></label>
-                    <input type="text" list="style-of-runner" className="form-input" name="styleOfRunner" onChange={changeHandler} />
-                    <datalist name="styleOfRunner" className="form-label" id="style-of-runner">
-                        <option value={form.styleOfRunner}>4" X 4"</option>
-                        <option value={form.styleOfRunner}>2" X 4"</option>
+                    <input type="text" 
+                        list="style-of-runner" 
+                        className="form-input" 
+                        name="styleOfRunner" 
+                        onChange={changeHandler} 
+                        value={form.styleOfRunner} />
+                    <datalist name="styleOfRunner" 
+                        className="form-label" 
+                        id="style-of-runner">
+                        <option value='4" X 4"'>4" X 4"</option>
+                        <option value='2" X 4"'>2" X 4"</option>
                     </datalist>
                 </div>
                 <div className="length-of-runner-container line-1-input">
                     <label htmlFor="length-of-runner" className="form-label">Length of Runner<br /></label>
-                    <input type="text" list="length-of-runner" className="form-input" name="lengthOfRunner" onChange={changeHandler} value={form.lengthOfRunner} />
+                    <input type="text" 
+                        list="length-of-runner" 
+                        className="form-input" 
+                        name="lengthOfRunner" 
+                        onChange={changeHandler} 
+                        value={form.lengthOfRunner} />
                     <datalist name="lengthOfRunner" className="form-label" id="length-of-runner">
-                        <option value={form.lengthOfRunner}>24"</option>
-                        <option value={form.lengthOfRunner}>36"</option>
-                        <option value={form.lengthOfRunner}>48"</option>
-                        <option value={form.lengthOfRunner}>60"</option>
-                        <option value={form.lengthOfRunner}>72"</option>
+                        <option value='24"'>24"</option>
+                        <option value='36"'>36"</option>
+                        <option value='48"'>48"</option>
+                        <option value='60"'>60"</option>
+                        <option value='72"'>72"</option>
                     </datalist>
                 </div>
                 <div className="qty-of-runner-container line-1-input">
                     <label htmlFor="qty-of-runner" className="form-label">Qty. of Runners<br /></label>
-                    <input type="number" className="form-input" name="qtyOfRunners" onChange={changeHandler} value={form.qtyOfRunners} />
+                    <input type="number" 
+                        className="form-input" 
+                        name="qtyOfRunners" 
+                        onChange={changeHandler} 
+                        value={form.qtyOfRunners} />
                 </div>
             </div>
             <div className="line-2">
@@ -90,14 +111,25 @@ const DesignYourPallet = () => {
                     </div> */}
                     <div className="required-pallet-certifications-container">
                         <label htmlFor="required-pallet-certifications" className="form-label">Required Pallet Certifications<br /></label>
-                        <select name="requiredPalletCertifications" className="form-input"          id="required-pallet-certifications" cols="30" rows="10" onChange={changeHandler}>
+                        <select name="requiredPalletCertifications" 
+                            className="form-input" 
+                            id="required-pallet-certifications" 
+                            cols="30" 
+                            rows="10" 
+                            onChange={changeHandler}>
                             <option>Select an option</option>
                             <option value="Export Stamped">Export Stamped</option>
                         </select>
                     </div>
                     <div className="special-notes-container">
                         <label htmlFor="special-notes" className="form-label">Special Notes<br /></label>
-                        <textarea name="specialNotes" className="form-input" id="special-notes" cols="30" rows="10" onChange={changeHandler} placeholder="Add any additional information about the pallet runners."></textarea>
+                        <textarea name="specialNotes" 
+                            className="form-input" 
+                            id="special-notes" cols="30" 
+                            rows="10" 
+                            onChange={changeHandler} 
+                            placeholder="Add any additional information about the pallet runners.">
+                        </textarea>
                     </div>
 
                 </div>
@@ -107,28 +139,40 @@ const DesignYourPallet = () => {
             <div className="line-5">
                 <div className="style-of-top-boards line-5-input">
                     <label htmlFor="style-of-top-boards" className="form-label">Style of Top Boards<br /></label>
-                    <input type="text" list="style-of-top-boards" className="form-input" name="styleOfRunner" onChange={changeHandler} />
+                    <input type="text" list="style-of-top-boards" className="form-input" name="styleOfTopBoards" onChange={changeHandler} />
                     <datalist name="styleOfTopBoards" className="form-label" id="style-of-top-boards">
-                        <option value={form.styleOfTopBoards}>4" X 7/16"</option>
-                        <option value={form.styleOfTopBoards}>4" X 1"</option>
-                        <option value={form.styleOfTopBoards}>6" X 7/16</option>
-                        <option value={form.styleOfTopBoards}>6" X 1"</option>
+                        <option value='4" X 7/16"'>4" X 7/16"</option>
+                        <option value='4" X 1"'>4" X 1"</option>
+                        <option value='6" X 7/16'>6" X 7/16"</option>
+                        <option value='6" X 1"'>6" X 1"</option>
                     </datalist>
                 </div>
                 <div className="qty-of-top-boards line-5-input">
                     <label htmlFor="qty-of-top-boards" className="form-label">Qty. of Top Boards<br /></label>
-                    <input type="number" list="qty-of-top-boards" className="form-input" name="qtyOfTopBoards" onChange={changeHandler} value={form.lengthOfRunner} />
+                    <input type="number" list="qty-of-top-boards" className="form-input" name="qtyOfTopBoards" onChange={changeHandler} value={form.qtyOfTopBoards} />
                 </div>
                 <div className="length-of-deck-boards-container line-5-input">
                     <label htmlFor="length-of-deck-boards" className="form-label">Length of Deck Boards<br /></label>
                     <input type="text" list="length-of-deck-boards" className="form-input" name="lengthOfDeckBoards" onChange={changeHandler} value={form.lengthOfDeckBoards} />
                     <datalist name="lengthOfDeckBoards" className="form-label" id="length-of-deck-boards">
-                        <option value={form.lengthOfDeckBoards}>40"</option>
-                        <option value={form.lengthOfDeckBoards}>42"</option>
-                        <option value={form.lengthOfDeckBoards}>45"</option>
-                        <option value={form.lengthOfDeckBoards}>48"</option>
-                        <option value={form.lengthOfDeckBoards}>54"</option>
-                        <option value={form.lengthOfDeckBoards}>68"</option>
+                        <option value='40"'>40"</option>
+                        <option value='42"'>42"</option>
+                        <option value='45"'>45"</option>
+                        <option value='48"'>48"</option>
+                        <option value='54"'>54"</option>
+                        <option value='68"'>68"</option>
+                    </datalist>
+                </div>
+            </div>
+            <div className="line-6">
+                <div className="style-of-bottom-boards line-6-input">
+                    <label htmlFor="style-of-bottom-boards" className="form-label">Style of Bottom Boards<br /></label>
+                    <input type="text" list="style-of-bottom-boards" className="form-input" name="styleOfBottomBoards" value={form.styleOfBottomBoards} onChange={changeHandler} />
+                    <datalist className="form-label" id="style-of-bottom-boards">
+                        <option value='4" X 7/16"'>4" X 7/16"</option>
+                        <option value='4" X 1"'>4" X 1"</option>
+                        <option value='6" X 7/16'>6" X 7/16</option>
+                        <option value='6" X 1"'>6" X 1"</option>
                     </datalist>
                 </div>
             </div>
